87e4844bdb077ce738cf79ca0b8cdb359d78b6a6
etbourreau <et.bourreau@laposte.net>
2017-07-12 14:26:00 +0100

    add bin + test error

---
+++ src/fr/pizzeria/bin/PizzeriaAdminConsoleApp.java
@@ -0,0 +1,26 @@
+package fr.pizzeria.bin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import fr.pizzeria.ihm.Menu;
+import fr.pizzeria.ihm.OptionMenu;
+import fr.pizzeria.model.Pizza;
+
+/**
+ * @author etbourreau
+ * Main Launcher class
+ */
+public class PizzeriaAdminConsoleApp {
+	
+	
+	static Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+	public static void main(String[] args) {
+		Glitch g = new Glitch("error");
+		
+		Menu mainMenu = new Menu();
+	}
+}
