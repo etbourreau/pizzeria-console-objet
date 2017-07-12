d3f4711a0a2cf8eadbd348b0b086f1002f393f83
etbourreau <et.bourreau@laposte.net>
2017-07-12 15:09:26 +0100

    add tests

---
+++ src/test/java/TestPizzaDao.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import main.java.fr.pizzeria.dao.PizzaDaoMemoire;
+
+public class TestPizzaDao {
+
+	public static void main(String[] args) {
+		PizzaDaoMemoire dao = new PizzaDaoMemoire();
+
+	}
+
+}
